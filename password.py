4d5edbe75eb1827cdcf99f2038c9cb260703a1db
reds362 <cbweng0924@yahoo.comtw>
2022-05-26 16:56:50 +0800

    update use while True

--- password.py
+++ password.py
@@ -1,17 +1,19 @@
 print('最多輸入3次密碼')
+password = 'a123456'
 i = 3
-while i >0:
-	password = input('請輸入密碼: ')
+while True:
 
-	if password == 'a123456':
+	pwd = input('請輸入密碼: ')
+	i = i-1
+
+	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		i = i-1
-		if i ==0:
+		if i == 0:
 			print('密碼錯誤!')
 			break
 		else:
-			print('密碼錯誤! 還有', i, '次機會')
+			print('還有', i, '次機會')
 
 	
